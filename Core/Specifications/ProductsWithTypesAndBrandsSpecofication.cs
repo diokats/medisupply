70d1e75f008fc56050ebd9cf7f8c4ad0aa8edab4
diokats <dioktys@gmail.com>
2023-09-12 11:21:05 +0300

    Add mapper

---
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecofication.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq.Expressions;
+using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class ProductsWithTypesAndBrandsSpecofication : BaseSpecification<Product>
+	{
+		public ProductsWithTypesAndBrandsSpecofication()
+		{
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+
+        public ProductsWithTypesAndBrandsSpecofication(int id) : base(x => x.Id == id)
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+    }
+}
+
