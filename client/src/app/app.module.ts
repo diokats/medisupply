abeda3bcafbcfb3d44f53f34417f10a48783bb9e
diokats <dioktys@gmail.com>
2023-09-24 20:23:50 +0300

    Routing-product-detail-home module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,18 +4,23 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CoreModule } from './core/core.module';
+import { ShopComponent } from './shop/shop.component';
+import { ShopModule } from './shop/shop.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule,
+    ShopModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
